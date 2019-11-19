9fe7bc18924a1495207b44c7d7defa169d54bf52
Chris Bail <christopher.bail@duke.edu>
2019-11-19 15:03:13 +0100

    update from class

--- Code_from_class/Humanitarian Advocacy Group Project.R
+++ Code_from_class/Humanitarian Advocacy Group Project.R
@@ -101,12 +101,117 @@ names(greenfollowstuff)
 #acqua alta in Italy.
 
 library(rtweet)
-acqua_alta_tweets<-search_tweets("venice flood", n=1000)
+acqua_alta_tweets<-search_tweets("flood in venice", n=1000)
 
 
 #view text of tweets to find hashtags
 acqua_alta_tweets$text
 
-#here is our crowd-sourced hashtags: https://docs.google.com/spreadsheets/d/1_yJJtv2MCA0QrJU3Drxin7NLQHSkowQw3LTHwMtQW3Y/edit?usp=sharing
+#here is our list of crowd-sourced hashtags: https://docs.google.com/spreadsheets/d/1_yJJtv2MCA0QrJU3Drxin7NLQHSkowQw3LTHwMtQW3Y/edit?usp=sharing
+
+hashtags<-read.csv("~/Documents/GitHub/comp_soc_undergrad/Code_from_class/hashtags.csv",
+                   stringsAsFactors = FALSE)
+
+our_hashtags<-as.character(hashtags$Acqua.ALta.Hashtags)
+
+
+#turns out we can't search for multiple hashtags at once,
+# so let's write a loop
+
+#to begin, let's remind ourselves how loops work
+#here's a loop that counts to three
+
+for(j in 1:3){
+  print(j)
+}
+
+#what we want to do is cycle through our list of hashtags and collect
+#tweets
+
+acqua_alta_data<-as.data.frame(NULL)
+
+for(i in 1:length(our_hashtags)){
+  #each time through the loop we search twitter for the hashtag
+  #we ask for 10,000 tweets that are not retweets and are recent (not popular)
+  venice_flood_tweets<-search_tweets(our_hashtags[i], 
+                                     n=10000,
+                                     include_rts=FALSE,
+                                     type="recent")
+  #then we bind each dataset to the dataset before it 
+  acqua_alta_data<-rbind(acqua_alta_data, venice_flood_tweets)
+  #we print the value of i to debug our code
+  print(i)
+  #we pause R each time through the loop to avoid rate limiting
+  Sys.sleep(3)
+}
+
+#we got rate limited, we need a longer Sys.sleep interval above
+
+#this line lets you check the rate limits
+myratelimits<-rate_limits()
+
+#first let's look at what languages we have
+table(acqua_alta_data$lang)
+
+#let's just analyze the English tweets
+
+english_tweets<-acqua_alta_data[acqua_alta_data$lang=="en",]
+
+library(syuzhet)
+
+english_tweets$sentiment<-get_sentiment(english_tweets$text)
+
+library(ggplot2)
+
+ggplot(english_tweets, aes(x=sentiment, y=retweet_count))+
+         geom_point()+
+         geom_smooth()+
+         theme_minimal()
+
+
+#let's create a variable that describes whether there is significant negative
+#tone to each tweet
+
+english_tweets$neg_tweet<-0
+english_tweets$neg_tweet[english_tweets$sentiment<0]<-1
+
+table(english_tweets$neg_tweet)
+
+english_tweets$followers_count
+
+our_model<-lm(retweet_count~
+    neg_tweet +
+    followers_count,
+    data=english_tweets
+    )
+
+summary(our_model)
+
+
+
+
+
+
+
+
+retweetcount= neg_tweet+ followers + error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
